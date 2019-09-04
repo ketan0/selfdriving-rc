d6c06185470452b1fae9c10ed9d8506209ac6f08
Ketan Agrawal <36572102+ketan0@users.noreply.github.com>
2019-09-04 13:48:45 -0400

    Update rctest.py

--- rctest.py
+++ rctest.py
@@ -7,10 +7,12 @@ from rccontrol import *
 from sklearn.preprocessing import StandardScaler
 
 camera = PiCamera()
-print "deserializing model..." #this takes ~1 min
+print("deserializing model...") #this takes ~1 min
+
+#NOTE: change this to the path of your model/scaler
 model = pickle.load( open('model2.p', 'rb') )
 scaler = pickle.load( open('scaler.p', 'rb') )
-print "begin test drive"
+print("begin test drive")
 
 def testDrive():
     while True:
